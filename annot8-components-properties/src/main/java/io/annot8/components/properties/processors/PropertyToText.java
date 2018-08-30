152d919966340664c77d0e787ef368f819d64c8f
James Baker <james.d.baker@gmail.com>
2018-08-30 15:44:21 +0100

    Blacklist and Whitelist configuration

--- annot8-components-properties/src/main/java/io/annot8/components/properties/processors/PropertyToText.java
+++ annot8-components-properties/src/main/java/io/annot8/components/properties/processors/PropertyToText.java
@@ -2,34 +2,82 @@ package io.annot8.components.properties.processors;
 
 import io.annot8.common.data.content.Text;
 import io.annot8.components.base.components.AbstractComponent;
+import io.annot8.components.properties.processors.PropertyToText.PropertyToTextSettings;
 import io.annot8.core.capabilities.CreatesContent;
 import io.annot8.core.components.Processor;
 import io.annot8.core.components.responses.ProcessorResponse;
+import io.annot8.core.context.Context;
 import io.annot8.core.data.Item;
 import io.annot8.core.exceptions.Annot8Exception;
+import io.annot8.core.exceptions.BadConfigurationException;
 import io.annot8.core.exceptions.IncompleteException;
+import io.annot8.core.exceptions.MissingResourceException;
 import io.annot8.core.exceptions.UnsupportedContentException;
+import io.annot8.core.settings.Settings;
+import io.annot8.core.settings.SettingsClass;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Convert properties on an item to separate Text content so they can be processed.
  * The toString() function is used to convert properties into a String.
  */
 @CreatesContent(Text.class)
+@SettingsClass(PropertyToTextSettings.class)
 public class PropertyToText extends AbstractComponent implements Processor {
+
+  private PropertyToTextSettings settings;
+
+  @Override
+  public void configure(Context context) throws BadConfigurationException, MissingResourceException {
+    super.configure(context);
+
+    settings = context.getSettings(PropertyToTextSettings.class)
+        .orElseGet(PropertyToTextSettings::new);
+  }
 
   @Override
   public ProcessorResponse process(Item item) throws Annot8Exception {
-    item.getProperties().getAll().forEach((k, v) -> {
-      try {
-        item.create(Text.class)
-          .withName(k)
-          .withData(v.toString())
-          .save();
-      }catch (UnsupportedContentException | IncompleteException e){
-        log().error("Unable to create Text content", e);
-      }
-    });
+    item.getProperties().getAll().entrySet().stream()
+      .filter(e -> !settings.getBlacklist().contains(e.getKey()))     //Key must not be on blacklist
+      .filter(e -> settings.getWhitelist().isEmpty() || settings.getWhitelist().contains(e.getKey())) //Key must be on whitelist, if it is set
+      .forEach(e -> {
+        try {
+          item.create(Text.class)
+            .withName(e.getKey())
+            .withData(e.getValue().toString())
+            .save();
+        }catch (UnsupportedContentException | IncompleteException ex){
+          log().error("Unable to create Text content", ex);
+        }
+      });
 
     return ProcessorResponse.ok();
+  }
+
+  public static class PropertyToTextSettings implements Settings {
+    private Set<String> whitelist = new HashSet<>();
+    private Set<String> blacklist = new HashSet<>();
+
+    public Set<String> getWhitelist() {
+      return whitelist;
+    }
+
+    public void setWhitelist(Set<String> whitelist) {
+      this.whitelist = whitelist;
+    }
+
+    public Set<String> getBlacklist() {
+      return blacklist;
+    }
+
+    public void setBlacklist(Set<String> blacklist) {
+      this.blacklist = blacklist;
+    }
+
+    @Override
+    public boolean validate() {
+      return true;
+    }
   }
 }
--- annot8-components-properties/src/test/java/io/annot8/components/properties/processors/PropertyToTextTest.java
+++ annot8-components-properties/src/test/java/io/annot8/components/properties/processors/PropertyToTextTest.java
@@ -5,19 +5,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import io.annot8.common.data.content.Text;
 import io.annot8.common.implementations.stores.AnnotationStoreFactory;
 import io.annot8.components.monitor.resources.Logging;
+import io.annot8.components.properties.processors.PropertyToText.PropertyToTextSettings;
 import io.annot8.core.components.Processor;
 import io.annot8.core.components.Resource;
 import io.annot8.core.context.Context;
 import io.annot8.core.data.Item;
 import io.annot8.core.data.ItemFactory;
 import io.annot8.core.exceptions.Annot8Exception;
+import io.annot8.core.settings.Settings;
 import io.annot8.defaultimpl.content.SimpleText;
 import io.annot8.defaultimpl.context.SimpleContext;
 import io.annot8.defaultimpl.data.SimpleItem;
 import io.annot8.defaultimpl.factories.SimpleContentBuilderFactoryRegistry;
 import io.annot8.defaultimpl.factories.SimpleItemFactory;
 import io.annot8.defaultimpl.stores.SimpleAnnotationStore;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.Test;
@@ -45,6 +49,92 @@ public class PropertyToTextTest {
       Item item = new SimpleItem(itemFactory, contentBuilderFactoryRegistry);
 
       item.getProperties().set("test", "Hello World!");
+
+      assertEquals(0, item.getContents().count());
+
+      p.process(item);
+
+      AtomicInteger count = new AtomicInteger();
+      item.getContents().forEach(c -> {
+            count.getAndIncrement();
+            assertEquals("test", c.getName());
+            assertEquals("Hello World!", c.getData());
+          }
+      );
+
+      assertEquals(1, count.get());
+
+    }
+  }
+
+  @Test
+  public void testWhitelist() throws Annot8Exception {
+    AnnotationStoreFactory factory = SimpleAnnotationStore.factory();
+    SimpleContentBuilderFactoryRegistry contentBuilderFactoryRegistry = new SimpleContentBuilderFactoryRegistry();
+    contentBuilderFactoryRegistry.register(Text.class, new SimpleText.BuilderFactory(factory));
+
+    ItemFactory itemFactory = new SimpleItemFactory(contentBuilderFactoryRegistry);
+
+    Logging logging = Logging.useLoggerFactory();
+    Map<String, Resource> resources = new HashMap<>();
+    resources.put("logging", logging);
+
+    Settings settings = new PropertyToTextSettings();
+    ((PropertyToTextSettings) settings).setWhitelist(new HashSet<>(Arrays.asList("test")));
+
+    Context context = new SimpleContext(itemFactory, Arrays.asList(settings), resources);
+
+    try(Processor p = new PropertyToText()) {
+
+      p.configure(context);
+
+      Item item = new SimpleItem(itemFactory, contentBuilderFactoryRegistry);
+
+      item.getProperties().set("test", "Hello World!");
+      item.getProperties().set("foo", "bar");
+
+      assertEquals(0, item.getContents().count());
+
+      p.process(item);
+
+      AtomicInteger count = new AtomicInteger();
+      item.getContents().forEach(c -> {
+            count.getAndIncrement();
+            assertEquals("test", c.getName());
+            assertEquals("Hello World!", c.getData());
+          }
+      );
+
+      assertEquals(1, count.get());
+
+    }
+  }
+
+  @Test
+  public void testBlacklist() throws Annot8Exception {
+    AnnotationStoreFactory factory = SimpleAnnotationStore.factory();
+    SimpleContentBuilderFactoryRegistry contentBuilderFactoryRegistry = new SimpleContentBuilderFactoryRegistry();
+    contentBuilderFactoryRegistry.register(Text.class, new SimpleText.BuilderFactory(factory));
+
+    ItemFactory itemFactory = new SimpleItemFactory(contentBuilderFactoryRegistry);
+
+    Logging logging = Logging.useLoggerFactory();
+    Map<String, Resource> resources = new HashMap<>();
+    resources.put("logging", logging);
+
+    Settings settings = new PropertyToTextSettings();
+    ((PropertyToTextSettings) settings).setBlacklist(new HashSet<>(Arrays.asList("foo")));
+
+    Context context = new SimpleContext(itemFactory, Arrays.asList(settings), resources);
+
+    try(Processor p = new PropertyToText()) {
+
+      p.configure(context);
+
+      Item item = new SimpleItem(itemFactory, contentBuilderFactoryRegistry);
+
+      item.getProperties().set("test", "Hello World!");
+      item.getProperties().set("foo", "bar");
 
       assertEquals(0, item.getContents().count());
 
