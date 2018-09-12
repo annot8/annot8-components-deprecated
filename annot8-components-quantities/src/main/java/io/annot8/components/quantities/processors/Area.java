d39a5fb544ca73bfc6e219d3fba92ce014ea811b
James Baker <james.d.baker@gmail.com>
2018-09-12 10:02:54 +0100

    Abstract base class for quantities, and Distance processor

--- annot8-components-quantities/src/main/java/io/annot8/components/quantities/processors/Area.java
+++ annot8-components-quantities/src/main/java/io/annot8/components/quantities/processors/Area.java
@@ -1,19 +1,9 @@
 package io.annot8.components.quantities.processors;
 
-import io.annot8.common.data.bounds.SpanBounds;
-import io.annot8.common.data.content.Text;
-import io.annot8.components.base.processors.AbstractTextProcessor;
 import io.annot8.conventions.AnnotationTypes;
-import io.annot8.conventions.PropertyKeys;
-import io.annot8.core.annotations.Annotation.Builder;
-import io.annot8.core.capabilities.CreatesAnnotation;
-import io.annot8.core.data.Item;
-import io.annot8.core.exceptions.Annot8Exception;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@CreatesAnnotation(value = AnnotationTypes.ANNOTATION_TYPE_AREA, bounds = SpanBounds.class)
-public class Area extends AbstractTextProcessor {
+public class Area extends AbstractQuantityProcessor {
 
   private static final double MM2_TO_M2 = 0.000001;
   private static final double CM2_TO_M2 = 0.0001;
@@ -68,64 +58,20 @@ public class Area extends AbstractTextProcessor {
           "\\b([0-9]+([0-9\\.,]+[0-9])?)[ ]?(thousand|million|billion|trillion)?[ ]?(acre)(s)?\\b",
           Pattern.CASE_INSENSITIVE);
 
+  public Area() {
+    super(AnnotationTypes.ANNOTATION_TYPE_AREA, "m^2");
 
-  @Override
-  protected void process(Item item, Text content) throws Annot8Exception {
-    process(content, mm2Pattern, MM2_TO_M2);
-    process(content, cm2Pattern, CM2_TO_M2);
-    process(content, m2Pattern, 1.0);
-    process(content, km2Pattern, KM2_TO_M2);
+    add(mm2Pattern, MM2_TO_M2);
+    add(cm2Pattern, CM2_TO_M2);
+    add(m2Pattern, 1.0);
+    add(km2Pattern, KM2_TO_M2);
 
-    process(content, in2Pattern, IN2_TO_M2);
-    process(content, ft2Pattern, FT2_TO_M2);
-    process(content, yd2Pattern, YD2_TO_M2);
-    process(content, mi2Pattern, MI2_TO_M2);
+    add(in2Pattern, IN2_TO_M2);
+    add(ft2Pattern, FT2_TO_M2);
+    add(yd2Pattern, YD2_TO_M2);
+    add(mi2Pattern, MI2_TO_M2);
 
-    process(content, acrePattern, ACRE_TO_M2);
-    process(content, haPattern, HECTARE_TO_M2);
-  }
-
-  private void process(Text content, Pattern pattern, double normalization) throws Annot8Exception{
-    Matcher m = pattern.matcher(content.getData());
-    while(m.find()){
-      Builder builder = content.getAnnotations().create()
-          .withType(AnnotationTypes.ANNOTATION_TYPE_AREA)
-          .withBounds(new SpanBounds(m.start(), m.end()));
-
-      try {
-        builder = builder
-          .withProperty(PropertyKeys.PROPERTY_KEY_VALUE,
-            normalise(m.group(1), m.group(2), normalization))
-          .withProperty(PropertyKeys.PROPERTY_KEY_UNIT, "m^2");
-      }catch (Exception e){
-        log().warn("Unable to parse and normalise value", e);
-      }
-
-      builder.save();
-    }
-  }
-
-  private double normalise(String number, String multiplier, double normalization){
-    double n = Double.parseDouble(number.replaceAll("[^0-9\\.]", ""));
-
-    long m = 1L;
-    if(multiplier != null) {
-      switch (multiplier.toLowerCase()) {
-        case "thousand":
-          m = 1000L;
-          break;
-        case "million":
-          m = 1000000L;
-          break;
-        case "billion":
-          m = 1000000000L;
-          break;
-        case "trillion":
-          m = 1000000000000L;
-          break;
-      }
-    }
-
-    return n*m*normalization;
+    add(acrePattern, ACRE_TO_M2);
+    add(haPattern, HECTARE_TO_M2);
   }
 }
--- annot8-components-quantities/src/test/java/io/annot8/components/quantities/processors/AreaTest.java
+++ annot8-components-quantities/src/test/java/io/annot8/components/quantities/processors/AreaTest.java
@@ -1,109 +1,70 @@
 package io.annot8.components.quantities.processors;
 
-import io.annot8.common.data.content.Text;
 import io.annot8.conventions.AnnotationTypes;
-import io.annot8.conventions.PropertyKeys;
-import io.annot8.core.annotations.Annotation;
-import io.annot8.core.components.Processor;
-import io.annot8.core.context.Context;
-import io.annot8.core.data.Item;
-import io.annot8.core.exceptions.Annot8Exception;
-import io.annot8.core.stores.AnnotationStore;
-import io.annot8.testing.testimpl.TestContext;
-import io.annot8.testing.testimpl.TestItem;
-import io.annot8.testing.testimpl.content.TestStringContent;
-import java.util.List;
-import java.util.stream.Collectors;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class AreaTest {
+public class AreaTest extends AbstractQuantityTest{
+
+  public AreaTest(){
+    super(Area.class, AnnotationTypes.ANNOTATION_TYPE_AREA, "m^2");
+  }
 
   @Test
-  public void testKM2() throws Annot8Exception {
+  public void testKM2() throws Exception {
     test("The region measured 800 square kilometres", "800 square kilometres", 8.0E8);
     test("The region measured 800 km^2", "800 km^2", 8.0E8);
   }
 
   @Test
-  public void testM2() throws Annot8Exception {
+  public void testM2() throws Exception {
     test("The field measured 400 square metres", "400 square metres", 400.0);
     test("The field measured 400 m^2", "400 m^2", 400.0);
   }
 
   @Test
-  public void testCM2() throws Annot8Exception {
+  public void testCM2() throws Exception {
     test("The table measured 200 square centimetres", "200 square centimetres", 0.02);
     test("The table measured 200 cm^2", "200 cm^2", 0.02);
   }
 
   @Test
-  public void testMM2() throws Annot8Exception {
+  public void testMM2() throws Exception {
     test("The chip measured 100 square millimetres", "100 square millimetres", 0.0001);
     test("The chip measured 100 mm^2", "100 mm^2", 0.0001);
   }
 
   @Test
-  public void testMi2() throws Annot8Exception {
+  public void testMi2() throws Exception {
     test("The region measured 800 square miles", "800 square miles", 2.07199048E9);
     test("The region measured 800 mi^2", "800 mi^2", 2.07199048E9);
   }
 
   @Test
-  public void testYd2() throws Annot8Exception {
+  public void testYd2() throws Exception {
     test("The field measured 400 square yards", "400 square yards", 334.450956);
     test("The field measured 400 yd^2", "400 yd^2", 334.450956);
   }
 
   @Test
-  public void testFt2() throws Annot8Exception {
+  public void testFt2() throws Exception {
     test("The table measured 100 square feet", "100 square feet", 9.2903044);
     test("The table measured 100 ft^2", "100 ft^2", 9.2903044);
   }
 
   @Test
-  public void testIn2() throws Annot8Exception {
+  public void testIn2() throws Exception {
     test("The chip measured 0.5 square inches", "0.5 square inches", 0.00032258);
     test("The chip measured 0.5 in^2", "0.5 in^2", 0.00032258);
   }
 
   @Test
-  public void testAcre() throws Annot8Exception {
+  public void testAcre() throws Exception {
     test("The field measured 400 acres", "400 acres", 1618742.56);
   }
 
   @Test
-  public void testHectare() throws Annot8Exception {
+  public void testHectare() throws Exception {
     test("The field measured 400 hectares", "400 hectares", 4000000.0);
-  }
-
-  private void test(String text, String expectedMatch, Double expectedValue) throws Annot8Exception {
-    try(
-        Processor p = new Area()
-    ) {
-      Item item = new TestItem();
-      Context context = new TestContext();
-
-      p.configure(context);
-
-      Text content = item.create(TestStringContent.class).withName("test")
-          .withData(text).save();
-
-      p.process(item);
-
-      AnnotationStore store = content.getAnnotations();
-
-      List<Annotation> annotations = store.getAll().collect(Collectors.toList());
-      Assertions.assertEquals(1, annotations.size());
-
-      Annotation a = annotations.get(0);
-      Assertions.assertEquals(AnnotationTypes.ANNOTATION_TYPE_AREA, a.getType());
-      Assertions.assertEquals(content.getId(), a.getContentId());
-      Assertions.assertEquals(expectedMatch, a.getBounds().getData(content).get());
-      Assertions.assertEquals(2, a.getProperties().getAll().size());
-      Assertions.assertEquals("m^2", a.getProperties().get(PropertyKeys.PROPERTY_KEY_UNIT).get());
-      Assertions.assertEquals(expectedValue, (Double)a.getProperties().get(PropertyKeys.PROPERTY_KEY_VALUE).get(), 0.000001);
-    }
   }
 
 }
