eb3ffd0eda70b7da968e71285ed442808d8e34e4
Chris Flatley <chris@committed.io>
2018-10-09 16:30:02 +0100

    Fix commented out test (effecting spotless)

--- annot8-components-db/src/main/java/io/annot8/components/db/processors/JDBCSettings.java
+++ annot8-components-db/src/main/java/io/annot8/components/db/processors/JDBCSettings.java
@@ -1,17 +1,18 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.components.db.processors;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+import com.google.common.base.Strings;
 import io.annot8.core.settings.Settings;
 
-public class JDBCSettings {
+public class JDBCSettings implements Settings {
 
-  private final String jdbcUrl;
-  private final String user;
-  private final String password;
+  private String jdbcUrl;
+  private String user;
+  private String password;
+
+  public JDBCSettings() {
+    this(null);
+  }
 
   public JDBCSettings(String jdbcUrl) {
     this(jdbcUrl, null, null);
@@ -20,6 +21,18 @@ public class JDBCSettings {
   public JDBCSettings(String jdbcUrl, String user, String password) {
     this.jdbcUrl = jdbcUrl;
     this.user = user;
+    this.password = password;
+  }
+
+  public void setJdbcUrl(String jdbcUrl) {
+    this.jdbcUrl = jdbcUrl;
+  }
+
+  public void setUser(String user) {
+    this.user = user;
+  }
+
+  public void setPassword(String password) {
     this.password = password;
   }
 
@@ -33,5 +46,10 @@ public class JDBCSettings {
 
   public String getUser() {
     return user;
+  }
+
+  @Override
+  public boolean validate() {
+    return !Strings.isNullOrEmpty(jdbcUrl);
   }
 }
