0904f3e3728e10a01f69f44d2315d8aad4d66a2e
Steven Taylor <steven@committed.io>
2018-10-09 13:09:07 +0100

    Adds processors to extract TableContent from database and CSV files

--- annot8-components-db/src/test/java/io/annot8/components/db/processors/JDBCSettingsTest.java
+++ annot8-components-db/src/test/java/io/annot8/components/db/processors/JDBCSettingsTest.java
@@ -1,25 +1,38 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.components.db.processors;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-class JDBCSettingsTest {
+public class JDBCSettingsTest {
+
+  private static final String JDBC_TEST_URL = "jdbc:hsqldb:hsql://localhost:9001/test";
+  private static final String USER = "TEST";
+  private static final String PASS = "TEST";
 
   @Test
-  public void test() {
-    JDBCSettings settings = new JDBCSettings();
+  public void testValidate() {
+    JDBCSettings settings = new JDBCSettings("jdbc:sqlite://exampleDB.db");
+    assertTrue(settings.validate());
+  }
 
-    settings.setJdbcUrl("jdbc");
-    settings.setPassword("pass");
-    settings.setUser("user");
+  @Test
+  public void testInvalidSettings() {
+    JDBCSettings settings = new JDBCSettings(null);
+    JDBCSettings emptySettings = new JDBCSettings("");
+    assertFalse(settings.validate());
+    assertFalse(emptySettings.validate());
+  }
 
-    assertThat(settings.getJdbcUrl()).isEqualTo("jdbc");
-    assertThat(settings.getPassword()).isEqualTo("pass");
-    assertThat(settings.getUser()).isEqualTo("user");
-
-    assertThat(settings.validate()).isTrue();
+  @Test
+  public void testCredentials() {
+    String user = "user";
+    String pass = "pass";
+    JDBCSettings settings = new JDBCSettings("jdbc::url", user, pass);
+    assertEquals(user, settings.getUser());
+    assertEquals(pass, settings.getPassword());
   }
 }
