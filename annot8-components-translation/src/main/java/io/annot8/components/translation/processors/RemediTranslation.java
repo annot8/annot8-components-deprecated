671a0a249990c5e5c0c7e6a265a270bfd19dd0c7
James Baker <james.d.baker@gmail.com>
2018-10-27 14:27:51 +0100

    Fix Spotless failures

--- annot8-components-translation/src/main/java/io/annot8/components/translation/processors/RemediTranslation.java
+++ annot8-components-translation/src/main/java/io/annot8/components/translation/processors/RemediTranslation.java
@@ -1,4 +1,9 @@
+/* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.components.translation.processors;
+
+import java.util.Optional;
+
+import uk.gov.nca.remedi4j.client.RemediClient;
 
 import io.annot8.common.data.content.Text;
 import io.annot8.components.base.processors.AbstractTextProcessor;
@@ -9,12 +14,11 @@ import io.annot8.core.data.Item;
 import io.annot8.core.exceptions.BadConfigurationException;
 import io.annot8.core.exceptions.MissingResourceException;
 import io.annot8.core.settings.SettingsClass;
-import java.util.Optional;
-import uk.gov.nca.remedi4j.client.RemediClient;
 
 /**
- * Use the REMEDI machine translation platform (see https://github.com/ivan-zapreev/Distributed-Translation-Infrastructure)
- * to perform translation of Text content objects.
+ * Use the REMEDI machine translation platform (see
+ * https://github.com/ivan-zapreev/Distributed-Translation-Infrastructure) to perform translation of
+ * Text content objects.
  */
 @CreatesContent(Text.class)
 @SettingsClass(RemediTranslationSettings.class)
@@ -37,10 +41,13 @@ public class RemediTranslation extends AbstractTextProcessor {
     source = opt.get().getSourceLanguage();
     target = opt.get().getTargetLanguage();
 
-    if(client != null)
-      client.close();
+    if (client != null) client.close();
 
-    client = new RemediClient(opt.get().getPreProcessorUri(), opt.get().getServerUri(), opt.get().getPostProcessorUri());
+    client =
+        new RemediClient(
+            opt.get().getPreProcessorUri(),
+            opt.get().getServerUri(),
+            opt.get().getPostProcessorUri());
   }
 
   @Override
@@ -56,12 +63,10 @@ public class RemediTranslation extends AbstractTextProcessor {
     } catch (Exception e) {
       log().error("Unable to translate text", e);
     }
-
   }
 
   @Override
   public void close() {
-    if(client != null)
-      client.close();
+    if (client != null) client.close();
   }
 }
