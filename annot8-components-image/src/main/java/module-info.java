32674550f0efb5d1b2f58cec62281439cf3c5b67
James Baker <james.d.baker@gmail.com>
2018-12-19 13:10:26 +0000

    Add missing export

--- annot8-components-image/src/main/java/module-info.java
+++ annot8-components-image/src/main/java/module-info.java
@@ -2,4 +2,6 @@ module annot8.components.image {
   requires io.annot8.components.base;
   requires io.annot8.common.data;
   requires metadata.extractor;
+
+  exports io.annot8.components.image.processors;
 }
