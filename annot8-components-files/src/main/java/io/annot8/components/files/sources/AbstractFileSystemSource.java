e49bcbe7fad8418ffa4968ed09a7754816286330
Chris Flatley <chris@committed.io>
2019-03-08 10:58:13 +0000

    no message

--- annot8-components-files/src/main/java/io/annot8/components/files/sources/AbstractFileSystemSource.java
+++ annot8-components-files/src/main/java/io/annot8/components/files/sources/AbstractFileSystemSource.java
@@ -1,14 +1,6 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.components.files.sources;
 
-import java.nio.file.Path;
-import java.time.Instant;
-import java.util.Collections;
-import java.util.Optional;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import io.annot8.common.data.content.FileContent;
 import io.annot8.components.base.components.AbstractComponent;
 import io.annot8.conventions.PropertyKeys;
@@ -17,9 +9,15 @@ import io.annot8.core.components.Source;
 import io.annot8.core.context.Context;
 import io.annot8.core.data.Item;
 import io.annot8.core.data.ItemFactory;
-import io.annot8.core.exceptions.Annot8Exception;
 import io.annot8.core.exceptions.BadConfigurationException;
 import io.annot8.core.exceptions.MissingResourceException;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @CreatesContent(FileContent.class)
 public abstract class AbstractFileSystemSource extends AbstractComponent implements Source {
@@ -75,8 +73,8 @@ public abstract class AbstractFileSystemSource extends AbstractComponent impleme
         item.create(FileContent.class).withName("file").withData(path.toFile()).save();
 
         return true;
-      } catch (Annot8Exception e) {
-        log().error("Unable to create item, discarding", e);
+      } catch (Throwable t) {
+        log().error("Unable to create item, discarding", t);
         item.discard();
       }
     }
