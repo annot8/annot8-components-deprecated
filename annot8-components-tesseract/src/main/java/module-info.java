cb57c20ee7e420d3e79ffdb3d1bf1beeee96373d
James Baker <james.d.baker@gmail.com>
2019-04-24 17:18:31 +0100

    Add module-info.java

---
+++ annot8-components-tesseract/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module io.annot8.components.tesseract {
+  requires transitive io.annot8.core;
+  requires io.annot8.common.data;
+  requires transitive io.annot8.components.base;
+  requires slf4j.api;
+  requires org.apache.commons.io;
+  requires tess4j;
+  requires java.desktop;
+}
